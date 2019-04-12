39455c2ae8acde507b1359e5ba8d9ae9b002f5ce
clbokea <clbo@kea.dk>
2019-04-12 08:05:55 +0200

    binary exercise

---
+++ exercises/google_interview_binary_search.py
@@ -0,0 +1,8 @@
+""" 
+    Holliday exercise:
+    In the video https://www.youtube.com/watch?v=XKu_SEDAykw&t=379s, the man suggest that a solution using a binary search approach wouuld be possible.
+    Your job is to code that solution. So listen carefully to the video and the create the solution. 
+    You can use the binary_search code we did in class as a base for your code.
+    https://github.com/python-elective-1-spring-2019/Lesson-13-Searching-Algorithms-Recursive-Fuctions/blob/master/code_examples/binary_search.py 
+
+"""
